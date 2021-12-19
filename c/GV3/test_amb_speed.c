a95f80cded3f33e64df4858bac41c86289ca52cb
detorresramos <detorresramos1@gmail.com>
2021-12-18 18:02:13 -0600

    working single csf access

--- c/GV3/test_amb_speed.c
+++ c/GV3/test_amb_speed.c
@@ -12,5 +12,17 @@
 
 
 int main(int argc, char** argv) {
-    printf("LMFAO\n");
+    int ch = open("../../data/ABCHeadlines_freq/testing/testing_M2/dump/csf1", O_RDONLY);
+    csf_t *csf = load_csf(ch);
+    int64_t val = csf3_get_byte_array(csf, "10", 2);
+
+    // #pragma loop through all csf, populate array
+    // use same seed for all csf and pass spooky into new lookup function
+
+    // cache efficient if all e's the same
+
+    // do hash table profiling in c++ with std::unordered_map
+    printf("%ld\n", val);
+    close(ch);
+    destroy_csf(csf);
 }
